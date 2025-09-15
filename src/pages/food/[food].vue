f048ae8777161f639c335d3f1b7334835ffaf554
Wern Ancheta <ancheta.wern@gmail.com>
2025-09-15 17:38:08 +0800

    [improvement] add citation

--- src/pages/food/[food].vue
+++ src/pages/food/[food].vue
@@ -672,6 +672,20 @@
       v-if="tourModeEnabled"
     />
 
+    <section class="mt-6 text-caption font-italic">
+        <p>Nutrition data sourced from the 
+            <a href="https://www.fnri.dost.gov.ph" target="_blank" rel="noopener">
+            Philippine Food Composition Table (FNRI-DOST)
+            </a> 
+            and 
+            <a href="https://fdc.nal.usda.gov/" target="_blank" rel="noopener">
+            USDA FoodData Central
+            </a>.
+        </p>
+    </section>
+
+
+
 </template>
 
 <script setup>
