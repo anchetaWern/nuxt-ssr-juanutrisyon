1bad6c49de31daac9f9cb8ed227d07da3283c1ef
Wern Ancheta <ancheta.wern@gmail.com>
2025-12-01 13:51:30 +0800

    [fix]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -115,18 +115,22 @@ export default defineNuxtConfig({
       background_color: '#ffffff',
       theme_color: '#84A42E',
       icons: [
-        {
-          src: '/icons/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
+      
         {
           src: '/icons/icon-512x512.png',
           sizes: '512x512',
           type: 'image/png'
         }
       ]
-    }
+    },
+
+    workbox: {
+     
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    
+    ssr: true,
+
   },
 
 
