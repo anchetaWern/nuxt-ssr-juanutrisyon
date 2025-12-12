9d8b391541bf606de6f5cc28389321ff6a3577a2
Wern Ancheta <ancheta.wern@gmail.com>
2025-12-12 09:56:42 +0800

    [refactor] move how to contribute to its own component

--- src/pages/food/[food].vue
+++ src/pages/food/[food].vue
@@ -463,18 +463,7 @@
 
     <DataSourceInfo />
     
-
-    <section class="mt-3 border-thin px-3 py-3">
-        <p class="text-caption">
-        Be part of the mission! 🇵🇭<br/>
-        We are building the most comprehensive Filipino food database but it’s only possible with your help. <br/>
-        Upload photos of food labels and help make healthier eating easier for everyone. <br/>
-        Click on the button below to start contributing.
-        While you're at it, please follow our <a href="https://www.facebook.com/juanutrisyon/" target="_blank">Facebook page</a> to stay updated of the latest development on this project. <br>
-        We are always open to ideas so feel free to reach out to us in there. 
-        </p>
-        <v-btn size="small" class="mt-2" color="primary" href="https://contribute.juanutrisyon.info">Contribute</v-btn>
-    </section>
+    <HowToContribute />
 
 
     </v-col>
@@ -535,6 +524,8 @@ import RecipeIngredients from '@/components/RecipeIngredients.vue';
 import DownloadNutritionLabel from '@/components/DownloadNutritionLabel.vue';
 
 import DataSourceInfo from '@/components/DataSourceInfo.vue';
+
+import HowToContribute from '@/components/HowToContribute.vue';
 
 const API_BASE_URI = import.meta.env.VITE_API_URI;
 
